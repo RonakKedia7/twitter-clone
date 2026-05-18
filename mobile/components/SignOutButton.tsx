a657fcd0e75694714b828c8de1c63ac3b65b6b8b
RonakKedia7 <ronakkedia0@gmail.com>
2026-05-18 16:39:22 +0545

    feat: update SignOutButton component; replace Feather icon with MaterialCommunityIcons and enhance styling

--- mobile/components/SignOutButton.tsx
+++ mobile/components/SignOutButton.tsx
@@ -1,13 +1,20 @@
-import { TouchableOpacity } from "react-native";
+import { TouchableOpacity, View } from "react-native";
 import React from "react";
-import { Feather } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { useSignOut } from "@/hooks/useSignOut";
 
 const SignOutButton = () => {
   const { handleSignOut } = useSignOut();
+
   return (
-    <TouchableOpacity onPress={handleSignOut}>
-      <Feather name="log-out" size={24} color={"#E0245E"} />
+    <TouchableOpacity
+      onPress={handleSignOut}
+      activeOpacity={0.7}
+      className="rounded-full"
+    >
+      <View className="bg-red-50 p-2 rounded-full border border-red-100">
+        <MaterialCommunityIcons name="logout" size={22} color="#E0245E" />
+      </View>
     </TouchableOpacity>
   );
 };
