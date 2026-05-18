85e13d79388acdc851e07e28e89f69b88e41b3b1
RonakKedia7 <ronakkedia0@gmail.com>
2026-05-18 15:15:32 +0545

    feat: update Android packaging configuration; add exclusion for MANIFEST.MF file

--- mobile/plugins/withAndroidPackaging.js
+++ mobile/plugins/withAndroidPackaging.js
@@ -4,13 +4,16 @@ module.exports = function withAndroidPackaging(config) {
   return withAppBuildGradle(config, (config) => {
     const buildGradle = config.modResults.contents;
 
-    if (!buildGradle.includes("META-INF/versions/9/OSGI-INF/MANIFEST")) {
+    if (!buildGradle.includes("META-INF/versions/9/OSGI-INF/MANIFEST.MF")) {
       config.modResults.contents = buildGradle.replace(
         /android\s*{/,
         `android {
     packaging {
         resources {
-            excludes += ["META-INF/versions/9/OSGI-INF/MANIFEST"]
+            excludes += [
+                "META-INF/versions/9/OSGI-INF/MANIFEST",
+                "META-INF/versions/9/OSGI-INF/MANIFEST.MF"
+            ]
         }
     }`,
       );
